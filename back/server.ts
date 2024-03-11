fa2f96fea1489308c3e8ac6a78347c9c8760c95e
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-03-11 22:43:20 +0200

    add server

---
+++ back/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = 4000;
+
+mongoose
+  .connect(process.env.MONGO_URL as string)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`App running on port ${port}`);
+    });
+  })
+  .catch((err: Error) => console.log(err.message));
