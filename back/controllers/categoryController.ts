ae716a2794308891be580d16d90b941ff3cd3ef2
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-03-12 23:16:19 +0200

    add categoryController

---
+++ back/controllers/categoryController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import Category from "../models/categoryModel";
+
+export const getAllCategories = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json({
+      status: "success",
+      results: categories.length,
+      data: {
+        categories,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
