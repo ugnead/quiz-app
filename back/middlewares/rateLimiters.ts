34a7075f61f5ebcbaddaae815aa79a6ee88c26ae
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-03-26 20:01:37 +0200

    update rate limiter

--- back/middlewares/rateLimiters.ts
+++ back/middlewares/rateLimiters.ts
@@ -1,4 +1,4 @@
-import rateLimit from 'express-rate-limit';
+import rateLimit from "express-rate-limit";
 
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -10,5 +10,6 @@ export const apiLimiter = rateLimit({
 export const loginLimiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   max: 5,
-  message: 'Too many login attempts from this IP, please try again after an hour',
+  message:
+    "Too many login attempts from this IP, please try again after an hour",
 });
