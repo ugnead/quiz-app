8c621f9f5dc2cffbc0e33c90a41b299ae02fb251
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-04-17 23:19:34 +0300

    fix api endpoint

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "http://localhost:4000/api/v1/",
 });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("jwtToken");
