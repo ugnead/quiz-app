64e4a62385ff3037a57207d0bfae3d069e6dccca
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-04-21 19:21:40 +0300

    update auth routes for refresh token

--- back/routes/authRoutes.ts
+++ back/routes/authRoutes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { googleAuth } from "../controllers/authController";
+import { handleRefreshToken } from "../controllers/tokenController";
 
 const router = express.Router();
 
 router.route("/google").post(googleAuth);
+router.route("/refresh_token").post(handleRefreshToken);
 
 export default router;
