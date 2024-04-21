3239fbfc98cee35f10efdc46351b3f59d5b6428d
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-04-21 19:22:47 +0300

    add auto refresh token to api

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { refreshAccessToken } from './refreshToken';
 
 const api = axios.create({
   baseURL: "http://localhost:4000/api/v1/",
@@ -10,6 +11,21 @@ api.interceptors.request.use((config) => {
   }
   return config;
 }, error => {
+  return Promise.reject(error);
+});
+
+api.interceptors.response.use((response) => response, async (error) => {
+  const originalRequest = error.config;
+  if (error.response.status === 401 && !originalRequest._retry) {
+    originalRequest._retry = true;
+    try {
+      await refreshAccessToken();
+      return api(originalRequest);
+    } catch (refreshError) {
+      console.error('Failed to refresh token:', refreshError);
+      return Promise.reject(refreshError);
+    }
+  }
   return Promise.reject(error);
 });
 
