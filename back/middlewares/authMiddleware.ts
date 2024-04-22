83e6ff46669ed1e05d40c932059fa85cfd4215ad
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-04-22 22:38:06 +0300

    update error status, fix env name

--- back/middlewares/authMiddleware.ts
+++ back/middlewares/authMiddleware.ts
@@ -7,17 +7,17 @@ export const verifyToken = (
   next: NextFunction
 ) => {
   const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split(" ")[1]; // Bearer TOKEN_HERE
+  const token = authHeader && authHeader.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ message: "No token provided" });
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+    const decoded = jwt.verify(token, process.env.JWT_TOKEN as string);
     (req as any).user = decoded;
     next();
   } catch (error) {
-    res.status(403).json({ message: "Invalid or expired token" });
+    res.status(401).json({ message: "Invalid or expired token" });
   }
 };
