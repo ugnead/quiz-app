e878c313999dae6e5cb4058e51f28b3cca403cca
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-05-16 19:31:43 +0300

    add question model

--- back/models/questionModel.ts
+++ back/models/questionModel.ts
@@ -1,19 +1,19 @@
 import { Document, Schema, model } from "mongoose";
 
 interface IQuestion extends Document {
-  category: Schema.Types.ObjectId;
   question: string;
   options: string[];
   correctAnswer: string;
   explanation?: string;
+  subcategory: Schema.Types.ObjectId;
 }
 
 let questionSchema = new Schema<IQuestion>({
-  category: { type: Schema.Types.ObjectId, ref: "Category" },
   question: { type: String, required: true },
   options: [{ type: String, required: true }],
   correctAnswer: { type: String, required: true },
   explanation: { type: String, required: false },
+  subcategory: { type: Schema.Types.ObjectId, ref: "Subcategory", required: true },
 });
 
 const Question = model<IQuestion>("Question", questionSchema);
