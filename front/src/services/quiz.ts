358b9a2bea80f7d2eb45c8bbf6f0500ad668b477
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-05-20 19:22:50 +0300

    add userProgress service

--- front/src/services/quiz.ts
+++ front/src/services/quiz.ts
@@ -1,7 +1,7 @@
 import api from "./api";
 
 export const fetchCategories = async () => {
-  const response = await api.get('/categories');
+  const response = await api.get("/categories");
   return response.data.data.categories;
 };
 
@@ -14,3 +14,21 @@ export const fetchQuestionsBySubcategory = async (subcategoryId: string) => {
   const response = await api.get(`/questions/subcategory/${subcategoryId}`);
   return response.data.data.questions;
 };
+
+export async function fetchQuestionsForLearning(subcategoryId: string) {
+  const response = await api.get(`/learn/${subcategoryId}`);
+  return response.data.data.questions;
+}
+
+export async function updateUserProgress(
+  questionId: string,
+  subcategoryId: string,
+  isCorrect: boolean
+) {
+  const response = await api.post(`/learn/user_progress`, {
+    questionId,
+    subcategoryId,
+    isCorrect,
+  });
+  return response.data;
+}
