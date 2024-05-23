ecf6b1b49f322a82ddade4afb8f8476857c9084f
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-05-23 19:37:36 +0300

    update routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import "./App.css";
 import "../src/styles/main.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Auth from "./components/Features/Auth";
-import { LoadingProvider } from "../src/contexts/LoadingContext";
+import CategoryList from "./components/Features/CategoryList";
+import SubcategoryList from "./components/Features/SubcategoryList";
+import Learn from "./components/Features/LearnQuestions";
+import ErrorBoundary from "./components/Common/ErrorBoundary";
 
 const App: React.FC = () => {
   return (
     <>
-      <LoadingProvider>
-        <Auth />
-      </LoadingProvider>
+      <ErrorBoundary>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Auth />} />
+            <Route path="/categories" element={<CategoryList />} />
+            <Route
+              path="/subcategories/:categoryId"
+              element={<SubcategoryList />}
+            />
+             <Route path="/learn/:subcategoryId" element={<Learn />} />
+          </Routes>
+        </Router>
+      </ErrorBoundary>
     </>
   );
 };
