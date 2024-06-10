b8b9b3b5f5e3fb10696e836ac705dd9ce01f5d12
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-06-10 23:31:35 +0300

    update subcategory list style

--- front/src/components/Features/SubcategoryList.tsx
+++ front/src/components/Features/SubcategoryList.tsx
@@ -7,6 +7,11 @@ interface Subcategory {
   name: string;
 }
 
+interface Category {
+  _id: string;
+  name: string;
+}
+
 const SubcategoryList: React.FC = () => {
   const { categoryId } = useParams<{ categoryId: string }>();
   const [subcategories, setSubcategories] = useState<Subcategory[]>([]);
@@ -20,11 +25,11 @@ const SubcategoryList: React.FC = () => {
           const [categoryData, subcategoryData] = await Promise.all([
             fetchCategoryById(categoryId),
             fetchSubcategories(categoryId)
-          console.error("Failed to fetch subcategories or category:", error);
+          ]);
           setCategory(categoryData);
           setSubcategories(subcategoryData);
         } catch (error) {
-          console.error("Failed to fetch subcategories:", error);
+          console.error("Failed to fetch subcategories or category:", error);
         }
       }
     };
@@ -33,12 +38,15 @@ const SubcategoryList: React.FC = () => {
   }, [categoryId]);
 
   return (
-    <div>
+    <div className="px-5 py-10">
       <h1 className="pb-12 text-center">{category?.name}</h1>
-      <ul>
+      <ul className="flex flex-col space-y-4">
         {subcategories.map((subcategory) => (
           <li key={subcategory._id}>
-            <button onClick={() => navigate(`/learn/${subcategory._id}`)}>
+            <button
+              className="w-80"
+              onClick={() => navigate(`/learn/${subcategory._id}`)}
+            >
               {subcategory.name}
             </button>
           </li>
