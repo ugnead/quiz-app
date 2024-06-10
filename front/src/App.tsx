62f2032e8438c5e7f11485884facc4a051c02840
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-06-10 23:30:07 +0300

    update Comp name

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,23 +5,27 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Auth from "./components/Features/Auth";
 import CategoryList from "./components/Features/CategoryList";
 import SubcategoryList from "./components/Features/SubcategoryList";
-import Learn from "./components/Features/LearnQuestions";
+import LearnQuestions from "./components/Features/LearnQuestions";
 import ErrorBoundary from "./components/Common/ErrorBoundary";
+import Navigation from "./components/Common/Navigation";
 
 const App: React.FC = () => {
   return (
     <>
       <ErrorBoundary>
         <Router>
-          <Routes>
-            <Route path="/" element={<Auth />} />
-            <Route path="/categories" element={<CategoryList />} />
-            <Route
-              path="/subcategories/:categoryId"
-              element={<SubcategoryList />}
-            />
-             <Route path="/learn/:subcategoryId" element={<Learn />} />
-          </Routes>
+          <Navigation />
+          <div className='h-max flex justify-center content-center'>
+            <Routes>
+              <Route path="/" element={<Auth />} />
+              <Route path="/categories" element={<CategoryList />} />
+              <Route
+                path="/subcategories/:categoryId"
+                element={<SubcategoryList />}
+              />
+              <Route path="/learn/:subcategoryId" element={<LearnQuestions />} />
+            </Routes>
+          </div>
         </Router>
       </ErrorBoundary>
     </>
