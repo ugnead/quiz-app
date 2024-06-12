103d6681fd8f1d99ddaa7231f166017956bd8383
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-06-12 21:26:22 +0300

    routes for learn, test and progress

--- back/routes/userProgressRoutes.ts
+++ back/routes/userProgressRoutes.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { getQuestionsForLearning, updateUserProgress } from "../controllers/userProgressController";
+import { getQuestionsForLearning, getQuestionsForTesting, updateUserProgress, getUserProgress } from "../controllers/userProgressController";
 
 const router = express.Router();
 
-router.route("/:subcategoryId").get(getQuestionsForLearning);
+router.route("/:subcategoryId/learn").get(getQuestionsForLearning);
+router.route("/:subcategoryId/test").get(getQuestionsForTesting);
+router.route("/:subcategoryId/progress").get(getUserProgress);
 router.route("/user_progress").post(updateUserProgress);
 
 export default router;
+
