a4cb4824dac49baec4fa81582a6753433701cd49
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-08-16 20:39:16 +0300

    .

--- front/src/components/Common/ReviewAnswer.tsx
+++ front/src/components/Common/ReviewAnswer.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 interface ReviewAnswerProps {
   answeredQuestions: {
     questionIndex: number;
     isCorrect: boolean;
-    selectedOption: string;
+    selectedOption: string | null;
     answerSequence: number;
   }[];
   handleReviewQuestion: (sequence: number) => void;
@@ -14,22 +14,30 @@ const ReviewAnswer: React.FC<ReviewAnswerProps> = ({
   answeredQuestions,
   handleReviewQuestion,
 }) => {
+  const firstButtonRef = useRef<HTMLButtonElement | null>(null);
+
+  useEffect(() => {
+    if (firstButtonRef.current) {
+      firstButtonRef.current.focus();
+    }
+  }, []);
+
   return (
     <div className="mt-4 flex flex-wrap">
       {answeredQuestions.map(({ answerSequence, isCorrect, selectedOption }, index) => {
-        let colorClass;
-        if (selectedOption === null) {
-          colorClass = "bg-yellow-500";
-        } else if (isCorrect) {
-          colorClass = "bg-green-500";
-        } else {
-          colorClass = "bg-red-500";
-        }
+        const colorClass = isCorrect
+          ? "bg-green-500"
+          : selectedOption === null
+          ? "bg-yellow-500"
+          : "bg-red-500";
+
         return (
           <button
             key={answerSequence}
+            ref={index === 0 ? firstButtonRef : null}
             onClick={() => handleReviewQuestion(answerSequence)}
             className={`w-11 mx-1 m-3 px-0 py-2 rounded-full text-white ${colorClass}`}
+            tabIndex={0}
           >
             {index + 1}
           </button>
