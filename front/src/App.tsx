28c88ade22b3a95770501766991f358e2f2e24ef
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-09-28 18:43:23 +0300

    render nav and categories components upon login

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import "./App.css";
 import "../src/styles/main.scss";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Features/Home";
 import CategoryList from "./components/Features/CategoryList";
 import SubcategoryList from "./components/Features/SubcategoryList";
@@ -13,38 +9,30 @@ import LearnQuestions from "./components/Features/LearnQuestions";
 import TestQuestions from "./components/Features/TestQuestions";
 import ErrorBoundary from "./components/Common/ErrorBoundary";
 import Navigation from "./components/Common/Navigation";
+import { useAuth } from "./contexts/AuthContext";
 
 const App: React.FC = () => {
-
-  const isHomePage = location.pathname === "/";
+  const { user } = useAuth();
 
   return (
-    <>
-      <ErrorBoundary>
-        <Router>
-          {!isHomePage && <Navigation />}
-          <div
-            className={
-              isHomePage ? "" : "flex justify-center mx-5 my-5 sm:my-10"
-            }
-          >
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/categories" element={<CategoryList />} />
-              <Route
-                path="/subcategories/:categoryId"
-                element={<SubcategoryList />}
-              />
-              <Route
-                path="/learn/:subcategoryId"
-                element={<LearnQuestions />}
-              />
-              <Route path="/test/:subcategoryId" element={<TestQuestions />} />
-            </Routes>
-          </div>
-        </Router>
-      </ErrorBoundary>
-    </>
+    <ErrorBoundary>
+      {user && <Navigation />}
+      <div className={user ? "flex justify-center mx-5 my-5 sm:my-10" : ""}>
+        <Routes>
+          <Route
+            path="/"
+            element={user ? <Navigate to="/categories" /> : <Home />}
+          />
+          <Route path="/categories" element={<CategoryList />} />
+          <Route
+            path="/subcategories/:categoryId"
+            element={<SubcategoryList />}
+          />
+          <Route path="/learn/:subcategoryId" element={<LearnQuestions />} />
+          <Route path="/test/:subcategoryId" element={<TestQuestions />} />
+        </Routes>
+      </div>
+    </ErrorBoundary>
   );
 };
 
