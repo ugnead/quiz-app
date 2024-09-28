82b31a9f435e45e3a5bf36db0b4fc1129e0b9ae6
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-09-28 18:39:35 +0300

    making router accesible globaly

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Router>
   </React.StrictMode>
 );
