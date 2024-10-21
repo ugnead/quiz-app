44e3e0b77fc2cd69f02523c1b1d5cca0e41c949e
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-10-21 21:46:01 +0300

    update subcategory routes

--- back/routes/subcategoryRoutes.ts
+++ back/routes/subcategoryRoutes.ts
@@ -1,9 +1,23 @@
-import express from 'express';
-import { getAllSubcategories, getSubcategoryById } from "../controllers/subcategoryController";
+import express from "express";
+import {
+  getAllSubcategories,
+  getSubcategoryById,
+  createSubcategory,
+  updateSubcategory,
+  deleteSubcategory,
+} from "../controllers/subcategoryController";
+import questionRouter from "./questionRoutes";
+import { isAdmin } from "../middlewares/isAdmin";
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
-router.route("/category/:categoryId").get(getAllSubcategories);
-router.route("/:subcategoryId").get(getSubcategoryById);
+router.get("/", getAllSubcategories);
+router.get("/:subcategoryId", getSubcategoryById);
+
+router.post("/", isAdmin, createSubcategory);
+router.put("/:subcategoryId", isAdmin, updateSubcategory);
+router.delete("/:subcategoryId", isAdmin, deleteSubcategory);
+
+router.use("/:subcategoryId/questions", questionRouter);
 
 export default router;
