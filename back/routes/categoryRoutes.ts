5eb3af62896dda5dd07d9d6c237a4771c0568807
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-10-21 21:43:20 +0300

    update category routes

--- back/routes/categoryRoutes.ts
+++ back/routes/categoryRoutes.ts
@@ -1,9 +1,23 @@
-import express from 'express';
-import { getAllCategories, getCategoryById } from "../controllers/categoryController";
+import express from "express";
+import {
+  getAllCategories,
+  getCategoryById,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+} from "../controllers/categoryController";
+import subcategoryRouter from "./subcategoryRoutes";
+import { isAdmin } from "../middlewares/isAdmin";
 
 const router = express.Router();
 
-router.route("/").get(getAllCategories);
-router.route("/:categoryId").get(getCategoryById);
+router.get("/", getAllCategories);
+router.get("/:categoryId", getCategoryById);
+
+router.post("/", isAdmin, createCategory);
+router.put("/:categoryId", isAdmin, updateCategory);
+router.delete("/:categoryId", isAdmin, deleteCategory);
+
+router.use("/:categoryId/subcategories", subcategoryRouter);
 
 export default router;
