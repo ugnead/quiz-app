11567c8608eafddcc92376984c1d1a5163f566e8
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-11-02 01:27:23 +0200

    add user controller

---
+++ back/controllers/userController.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from "express";
+import User, { IUser } from "../models/userModel";
+
+export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const users: IUser[] = await User.find();
+    res.status(200).json({
+      status: "success",
+      results: users.length,
+      data: {
+        users,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: error || "Internal Server Error",
+    });
+  }
+};
+
+export const updateUserRole = async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.params;
+    const { role } = req.body;
+
+    if (!["user", "admin"].includes(role)) {
+      return res.status(400).json({
+        status: "fail",
+        message: 'Invalid role. Must be "user" or "admin".',
+      });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { role },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({
+        status: "fail",
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        user: updatedUser,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: error || "Internal Server Error",
+    });
+  }
+};
