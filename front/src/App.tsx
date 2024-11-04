786071da77ce3595b10206c514a0da6f423df203
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-11-04 22:17:38 +0200

    restructure for public and private routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,37 +1,71 @@
 import React from "react";
-import "./App.css";
-import "../src/styles/main.scss";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "./components/Features/Home";
-import CategoryList from "./components/Features/CategoryList";
-import SubcategoryList from "./components/Features/SubcategoryList";
-import LearnQuestions from "./components/Features/LearnQuestions";
-import TestQuestions from "./components/Features/TestQuestions";
+import Home from "./components/Public/Home";
+import CategoryList from "./components/User/CategoryList";
+import SubcategoryList from "./components/User/SubcategoryList";
+import LearnQuestions from "./components/User/LearnQuestions";
+import TestQuestions from "./components/User/TestQuestions";
 import ErrorBoundary from "./components/Common/ErrorBoundary";
-import Navigation from "./components/Common/Navigation";
 import { useAuth } from "./contexts/AuthContext";
+import PublicLayout from "./components/Layouts/PublicLayout";
+import UserLayout from "./components/Layouts/UserLayout";
+import AdminLayout from "./components/Layouts/AdminLayout";
 
 const App: React.FC = () => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return null;
+  }
 
   return (
     <ErrorBoundary>
-      {user && <Navigation />}
-      <div className={user ? "flex justify-center mx-5 my-5 sm:my-10" : ""}>
-        <Routes>
-          <Route
-            path="/"
-            element={user ? <Navigate to="/categories" /> : <Home />}
-          />
-          <Route path="/categories" element={<CategoryList />} />
-          <Route
-            path="/categories/:categoryId/subcategories"
-            element={<SubcategoryList />}
-          />
-          <Route path="/subcategories/:subcategoryId/questions/learn" element={<LearnQuestions />} />
-          <Route path="/subcategories/:subcategoryId/questions/test" element={<TestQuestions />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+
+        {user && (
+          <Route element={<UserLayout />}>
+            <Route path="/categories" element={<CategoryList />} />
+            <Route
+              path="/categories/:categoryId/subcategories"
+              element={<SubcategoryList />}
+            />
+            <Route
+              path="/subcategories/:subcategoryId/questions/learn"
+              element={<LearnQuestions />}
+            />
+            <Route
+              path="/subcategories/:subcategoryId/questions/test"
+              element={<TestQuestions />}
+            />
+          </Route>
+        )}
+
+        {user && user.role === "admin" && (
+          <Route path="/admin/*" element={<AdminLayout />}>
+            <Route path="categories" element={<CategoryList />} />
+            <Route path="subcategories" element={<SubcategoryList />} />
+            <Route path="questions" element={<SubcategoryList />} />
+          </Route>
+        )}
+
+        <Route
+          path="*"
+          element={
+            user ? (
+              user.role === "admin" ? (
+                <Navigate to="/admin/categories" />
+              ) : (
+                <Navigate to="/categories" />
+              )
+            ) : (
+              <Navigate to="/" />
+            )
+          }
+        />
+      </Routes>
     </ErrorBoundary>
   );
 };
