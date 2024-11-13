7a155d5d8dd8029312ea686f9315461d9a8c441b
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-11-13 19:14:51 +0200

    .

--- front/src/components/Layouts/AdminLayout.tsx
+++ front/src/components/Layouts/AdminLayout.tsx
@@ -2,25 +2,32 @@ import React from "react";
 import Navigation from "../Common/Navigation";
 import Sidebar from "../Common/Sidebar";
 import { Outlet } from "react-router-dom";
+import { FaQuestionCircle, FaUser } from "react-icons/fa";
 
-const AdminLayout: React.FC = () => {
-  const adminNavItems = [
-    { label: "Dashboard", path: "/admin/dashboard" },
-    { label: "Categories", path: "/admin/categories" },
-    { label: "Subcategories", path: "/admin/subcategories" },
-    { label: "Questions", path: "/admin/questions" },
-    { label: "Users", path: "/admin/users" },
-  ];
+const navItems = [
+  { label: "Quiz", path: "/admin/categories", icon: FaQuestionCircle },
+  { label: "Users", path: "/admin/users", icon: FaUser },
+];
+
+const Layout: React.FC = () => {
 
   return (
-    <div className="admin-container">
+    <>
       <Navigation />
-      <Sidebar items={adminNavItems} className="admin-sidebar" />
-      <div className="admin-content">
-        <Outlet />
+      <div className="flex h-screen">
+        <Sidebar
+          items={navItems}
+          title="Admin Panel"
+        />
+
+        <div className="flex-1 p-5 sm:p-10">
+          <div className="justify-items-center">
+            <Outlet />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default AdminLayout;
+export default Layout;
