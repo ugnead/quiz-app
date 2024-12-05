c0a1ff4eb7d7f6cba9af3a9ff8b4a2f358523270
Ugne Adomaityte <u.adomaityte98@gmail.com>
2024-12-05 23:57:23 +0200

    .

--- front/src/components/Common/Form/Fields/Select.tsx
+++ front/src/components/Common/Form/Fields/Select.tsx
@@ -41,7 +41,6 @@ const Select: React.FC<SelectProps> = ({
         <div className="px-3 py-2 bg-gray-100 border border-gray-300 rounded-md">
           {selectedOption ? selectedOption.label : ""}
         </div>
-        {/* Hidden input to include the value in form submission */}
         <input type="hidden" name={props.name} value={props.value} />
       </div>
     );
