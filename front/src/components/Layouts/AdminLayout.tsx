7e23abe1ead819f4fffd083eed8ab9bef3b9047a
Ugne Adomaityte <u.adomaityte98@gmail.com>
2025-01-01 22:43:26 +0200

    redo routes

--- front/src/components/Layouts/AdminLayout.tsx
+++ front/src/components/Layouts/AdminLayout.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import Navigation from "../Common/Navigation";
 import Sidebar from "../Common/Sidebar";
-import { Outlet } from "react-router-dom";
 import { FaQuestionCircle, FaUser } from "react-icons/fa";
+import AdminCategoryList from "../Admin/CategoryList";
+import AdminSubcategoryList from "../Admin/SubcategoryList";
+import UserList from "../Admin/UserList";
+import Button from "../Common/Button";
 
 const navItems = [
   { label: "Quiz", path: "/admin/categories", icon: FaQuestionCircle },
@@ -10,19 +14,36 @@ const navItems = [
 ];
 
 const Layout: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <>
-    <div className="flex flex-col min-h-screen h-auto">
-      <Navigation />
-      <div className="flex flex-1">
-        <Sidebar items={navItems} title="Admin Panel" />
+      <div className="flex flex-col min-h-screen h-auto">
+        <Navigation />
+        <div className="flex flex-1">
+          <Sidebar items={navItems} title="Admin Panel" />
 
-        <div className="flex-1 p-5 pb-10 sm:p-10 min-w-0">
-          <div className="w-full max-w-4xl mx-auto">
-            <Outlet />
+          <div className="flex-1 p-5 pb-10 sm:p-10 min-w-0">
+            <div className="w-full max-w-4xl mx-auto">
+              <Routes>
+                <Route index element={<Navigate to="categories" replace />} />
+                <Route path="categories" element={<AdminCategoryList />} />
+                <Route
+                  path="categories/:categoryId/subcategories"
+                  element={<AdminSubcategoryList />}
+                />
+                <Route path="users" element={<UserList />} />
+
+                <Route
+                  path="*"
+                  element={<Navigate to="categories" replace />}
+                />
+              </Routes>
+              <Button onClick={() => navigate("/quiz/categories")}>
+                User Panel
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
       </div>
     </>
   );
--- front/src/components/Layouts/UserLayout.tsx
+++ front/src/components/Layouts/UserLayout.tsx
@@ -1,13 +1,34 @@
 import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Navigation from "../Common/Navigation";
-import { Outlet } from "react-router-dom";
+import CategoryList from "../User/CategoryList";
+import SubcategoryList from "../User/SubcategoryList";
+import LearnQuestions from "../User/LearnQuestions";
+import TestQuestions from "../User/TestQuestions";
 
 const UserLayout: React.FC = () => {
   return (
     <>
       <Navigation />
       <div className="flex justify-center mx-5 my-5 sm:my-10">
-        <Outlet />
+        <Routes>
+          <Route index element={<Navigate to="categories" replace />} />
+          <Route path="categories" element={<CategoryList />} />
+          <Route
+            path="categories/:categoryId/subcategories"
+            element={<SubcategoryList />}
+          />
+          <Route
+            path="subcategories/:subcategoryId/questions/learn"
+            element={<LearnQuestions />}
+          />
+          <Route
+            path="subcategories/:subcategoryId/questions/test"
+            element={<TestQuestions />}
+          />
+
+          <Route path="*" element={<Navigate to="categories" replace />} />
+        </Routes>
       </div>
     </>
   );
