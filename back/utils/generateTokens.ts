2be74af5371878aaba746fef478103e049bb4c9c
Ugne Adomaityte <u.adomaityte98@gmail.com>
2025-01-04 17:42:37 +0200

    refactor token generation into utility function

---
+++ back/utils/generateTokens.ts
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+interface TokenPayload {
+  userId: string;
+  name: string;
+  email: string;
+  role: string;
+}
+
+export const generateTokens = (userPayload: TokenPayload) => {
+
+  const accessToken = jwt.sign(userPayload, process.env.JWT_TOKEN!, {
+    expiresIn: "5s",
+  });
+
+  const refreshToken = jwt.sign(
+    { userId: userPayload.userId },
+    process.env.REFRESH_TOKEN!,
+    { expiresIn: "7d" }
+  );
+
+  return { accessToken, refreshToken };
+};
