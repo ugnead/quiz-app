46b03631642d49032fedea294e50df546267d03b
Ugne Adomaityte <u.adomaityte98@gmail.com>
2025-01-05 18:18:29 +0200

    additional checks for the question name

--- back/models/questionModel.ts
+++ back/models/questionModel.ts
@@ -11,7 +11,12 @@ interface IQuestion extends Document {
 }
 
 let questionSchema = new Schema<IQuestion>({
-  name: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+    minlength: [1, "Name must be at least 1 character(s) long"],
+    maxlength: [50, "Name cannot exceed 50 character(s)"],
+  },
   options: [{ type: String, required: true }],
   correctAnswer: { type: String, required: true },
   explanation: { type: String, required: false },
