7975ec6e6e4fde1111376c10eb1b91c7471f8464
Ugne Adomaityte <u.adomaityte98@gmail.com>
2025-01-10 20:48:20 +0200

    limit answerOptions to 4

--- back/validators/questionSchemas.ts
+++ back/validators/questionSchemas.ts
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const createQuestionSchema = Joi.object({
   name: Joi.string().trim().min(3).max(255).required(),
-  answerOptions: Joi.array().items(Joi.string().required()).min(2).required(),
+  answerOptions: Joi.array().items(Joi.string().required()).min(2).max(4).required(),
   correctAnswer: Joi.string().required(),
   explanation: Joi.string().trim().max(255).allow("", null),
   status: Joi.string().valid("enabled", "disabled").default("enabled"),
