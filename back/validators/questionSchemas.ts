0489188083cfdc06c5e8791e4da3cfa905d08f91
Ugne Adomaityte <u.adomaityte98@gmail.com>
2025-01-10 21:03:35 +0200

    add additional checks

--- back/validators/questionSchemas.ts
+++ back/validators/questionSchemas.ts
@@ -2,16 +2,16 @@ import Joi from "joi";
 
 export const createQuestionSchema = Joi.object({
   name: Joi.string().trim().min(3).max(255).required(),
-  answerOptions: Joi.array().items(Joi.string().required()).min(2).max(4).required(),
-  correctAnswer: Joi.string().required(),
-  explanation: Joi.string().trim().max(255).allow("", null),
+  answerOptions: Joi.array().items(Joi.string().trim().min(1).max(255).required()).min(2).max(4).required(),
+  correctAnswer: Joi.string().trim().min(1).max(255).required(),
+  explanation: Joi.string().trim().min(3).max(500).optional(),
   status: Joi.string().valid("enabled", "disabled").default("enabled"),
 });
 
 export const updateQuestionSchema = Joi.object({
   name: Joi.string().trim().min(3).max(255).optional(),
-  answerOptions: Joi.array().items(Joi.string().required()).min(2).optional(),
-  correctAnswer: Joi.string().optional(),
-  explanation: Joi.string().trim().max(255).allow("", null).optional(),
+  answerOptions: Joi.array().items(Joi.string().trim().min(1).max(255).required()).min(2).max(4).optional(),
+  correctAnswer: Joi.string().trim().min(1).max(255).optional(),
+  explanation: Joi.string().trim().min(3).max(500).optional(),
   status: Joi.string().valid("enabled", "disabled").optional(),
 });
