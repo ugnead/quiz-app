7b00a452b5cd942d0082b45a8159dfdcd3ff8429
Ugne Adomaityte <u.adomaityte98@gmail.com>
2025-01-12 22:19:03 +0200

    feature: possibility to add footer

--- front/src/components/Common/Sidebar.tsx
+++ front/src/components/Common/Sidebar.tsx
@@ -12,9 +12,10 @@ interface NavItem {
 interface SidebarProps {
   items: NavItem[];
   title?: string;
+  footer?: React.ReactNode;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ items, title }) => {
+const Sidebar: React.FC<SidebarProps> = ({ items, title, footer }) => {
   const [isOpen, setIsOpen] = useState(window.innerWidth >= 768);
 
   useEffect(() => {
@@ -44,32 +45,39 @@ const Sidebar: React.FC<SidebarProps> = ({ items, title }) => {
         className={`max-md:h-full bg-white text-black w-60 z-40 fixed top-0 left-0 md:relative duration-500 ease-in-out border-r
           ${isOpen ? "translate-x-0" : "-translate-x-full"}`}
       >
-        <nav className="px-4 py-10">
-          {title && (
-            <div className="text-center mb-7">
-              <h3>{title}</h3>
-            </div>
+        <div className="flex flex-col h-full">
+          <nav className="px-4 py-10 flex-1 overflow-auto">
+            {title && (
+              <div className="text-center mb-7">
+                <h3>{title}</h3>
+              </div>
+            )}
+            <ul>
+              {items.map((item) => (
+                <li key={item.path}>
+                  <NavLink
+                    to={item.path}
+                    className={({ isActive }) =>
+                      `flex items-center mb-2 py-2 px-4 rounded ${
+                        isActive
+                          ? "bg-gray-200"
+                          : "hover:bg-gray-100 transition-colors"
+                      }`
+                    }
+                  >
+                    <item.icon className="mr-3 text-black" size={18} />
+                    {item.label}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+
+          {footer && (
+            <div className="p-4">{footer}</div>
           )}
-          <ul>
-            {items.map((item) => (
-              <li key={item.path}>
-                <NavLink
-                  to={item.path}
-                  className={({ isActive }) =>
-                    `flex items-center mb-2 py-2 px-4 rounded ${
-                      isActive
-                        ? "bg-gray-200"
-                        : "hover:bg-gray-100 transition-colors"
-                    }`
-                  }
-                >
-                  <item.icon className="mr-3 text-black" size={18} />
-                  {item.label}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
+        </div>
+
         <button
           className={`md:hidden absolute top-[70px] sm:top-[88px] right-[-15px] sm:right-[-35px] translate-x-full text-black p-2 z-50 bg-gray-100 rounded-full
             ${isOpen ? "hidden" : ""}`}
