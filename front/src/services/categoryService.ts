fe8cb813b4aee60cf808ca723c1f5c99141afa44
Ugne Adomaityte <u.adomaityte98@gmail.com>
2025-01-17 22:08:22 +0200

    use stricter typing in category services

--- front/src/services/categoryService.ts
+++ front/src/services/categoryService.ts
@@ -1,29 +1,34 @@
 import api from "./api";
+import { Category, CreateCategoryDto, UpdateCategoryDto } from "../types";
 
-export const fetchCategories = async () => {
+export const fetchCategories = async (): Promise<Category[]> => {
   const response = await api.get("/categories");
-  return response.data.data.categories;
+  return response.data.data.categories as Category[];
 };
 
-export const fetchCategoryById = async (categoryId: string) => {
+export const fetchCategoryById = async (
+  categoryId: string
+): Promise<Category> => {
   const response = await api.get(`/categories/${categoryId}`);
-  return response.data.data.category;
+  return response.data.data.category as Category;
 };
 
-export const createCategory = async (categoryData: Record<string, string>) => {
+export const createCategory = async (
+  categoryData: CreateCategoryDto
+): Promise<Category> => {
   const response = await api.post("/categories", categoryData);
   return response.data.data.category;
 };
 
 export const updateCategory = async (
   categoryId: string,
-  categoryData: Record<string, string>
-) => {
+  categoryData: UpdateCategoryDto
+): Promise<Category> => {
   const response = await api.patch(`/categories/${categoryId}`, categoryData);
   return response.data.data.category;
 };
 
-export const deleteCategory = async (categoryId: string) => {
+export const deleteCategory = async (categoryId: string): Promise<void> => {
   const response = await api.delete(`/categories/${categoryId}`);
   return response.data;
 };
