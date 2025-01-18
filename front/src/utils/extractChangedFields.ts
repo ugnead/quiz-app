97140625ac2812892bad12a91daf67be5431cf42
Ugne Adomaityte <u.adomaityte98@gmail.com>
2025-01-18 18:23:01 +0200

    refactor extractChangedFields to return a Partial type

--- front/src/utils/extractChangedFields.ts
+++ front/src/utils/extractChangedFields.ts
@@ -1,12 +1,12 @@
 export function extractChangedFields<T extends Record<string, string | string[]>>(
-    oldVals: T,
-    newVals: T
-  ): T {
-    const changedFields = {} as T;
-    for (const key in newVals) {
-      if (newVals[key] !== oldVals[key]) {
-        changedFields[key] = newVals[key];
-      }
+  oldVals: T,
+  newVals: T
+): Partial<T> {
+  const changedFields = {} as Partial<T>;
+  for (const key in newVals) {
+    if (newVals[key] !== oldVals[key]) {
+      changedFields[key] = newVals[key];
     }
-    return changedFields;
   }
+  return changedFields;
+}
