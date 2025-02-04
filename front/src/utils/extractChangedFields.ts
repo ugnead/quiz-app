a19a4570e0d26ed620a7eeec297c398d1b019ae6
Ugne Adomaityte <u.adomaityte98@gmail.com>
2025-02-04 21:35:30 +0200

    .

--- front/src/utils/extractChangedFields.ts
+++ front/src/utils/extractChangedFields.ts
@@ -1,7 +1,6 @@
-export function extractChangedFields<T extends Record<string, string | string[]>>(
-  oldVals: T,
-  newVals: T
-): Partial<T> {
+export function extractChangedFields<
+  T extends Record<string, string | string[]>,
+>(oldVals: T, newVals: T): Partial<T> {
   const changedFields = {} as Partial<T>;
   for (const key in newVals) {
     if (JSON.stringify(newVals[key]) !== JSON.stringify(oldVals[key])) {
